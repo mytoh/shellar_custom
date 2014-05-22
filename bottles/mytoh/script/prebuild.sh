2b22acc452cdb12bcdc669fc1752488cd8b99a61
mytoh <mytoh@gmail.com>
2014-05-23 00:24:49 +0900

    * bottles/mytoh/script/prebuild.sh: add ports rebuild shell

---
+++ bottles/mytoh/script/prebuild.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+set -o nounset
+set -o errexit
+
+
+rebuild_run_deps() {
+    local dir="/usr/ports/${1}"
+    local depends="$(make -C ${dir} run-depends-list)"
+    for p in ${depends}
+    do
+        sudo make -s -C ${p} clean reinstall clean distclean
+    done
+}
+
+rebuild_all_deps() {
+    local dir="/usr/ports/${1}"
+    local depends="$(make -C ${dir} all-depends-list)"
+    for p in ${depends}
+    do
+        sudo make -s -C ${p} reinstall clean distclean
+    done
+}
+
+rebuild_build_all() {
+    local dir="/usr/ports/${1}"
+    local depends="$(make -C ${dir} build-depends-list)"
+    for p in ${depends}
+    do
+        sudo make -C ${p} reinstall clean distclean
+    done
+}
+
+rebuild_one() {
+    local dir="/usr/ports/${1}"
+
+    sudo make -s -C ${dir} clean reinstall clean distclean
+}
+
+main() {
+    local command target
+    command="${1}"
+    target="${2}"
+    case ${command} in
+        run)
+            rebuild_run_deps "${target}"
+            rebuild_one "${target}" ;;
+        build)
+            rebuild_build_deps "${target}"
+            rebuild_one "${target}" ;;
+        all)
+            rebuild_all_deps "${target}"
+            rebuild_one "${target}" ;;
+        *) echo no such command ;;
+    esac
+}
+
+main "${@}"
