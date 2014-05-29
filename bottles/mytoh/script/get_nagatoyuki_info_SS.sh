2709e4960c0e2dde84da322344ed2acada624b1b
mytoh <mytoh@gmail.com>
2014-05-30 02:32:35 +0900

    * bottles/mytoh/script/get_nagatoyuki_info_SS.sh: fix

--- bottles/mytoh/script/get_nagatoyuki_info_SS.sh
+++ bottles/mytoh/script/get_nagatoyuki_info_SS.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
 get_html() {
-    local num="${1}"
-    local orig="SS${num}.orig"
+    local num orig
+    num="${1}"
+    orig="SS${num}.orig"
 
     fetch -o ${orig} "http://nagatoyuki.info/?SS%BD%B8%2F${num}"
 }
 
 convert() {
-    local num="${1}"
-    local orig="SS${num}.orig"
-    local html="SS${num}.html"
+    local num orig html
+    num="${1}"
+    orig="SS${num}.orig"
+    html="SS${num}.html"
 
     nkf -w ${orig} > ${html}
     rm ${orig}
@@ -19,9 +21,10 @@ convert() {
 }
 
 remove_space() {
-    local num="${1}"
-    local org="SS${num}.org"
-    local sedf="SS${num}.org.seded"
+    local num org sedf
+    num="${1}"
+    org="SS${num}.org"
+    sedf="SS${num}.org.seded"
 
     gsed -E 's/\s+$//g'  ${org} > ${sedf}
 
@@ -29,11 +32,17 @@ remove_space() {
     mv ${sedf} ${org}
 }
 
-main() {
+is_exists() {
+    local file
+    file="${1}"
+    test -e ${file}
+}
 
+main() {
+    local num
     for num in $(seq 1 1200)
     do
-        if test ! -e SS${num}.html
+        if ! is_exists SS${num}.html
         then
             get_html ${num}
             convert ${num}
