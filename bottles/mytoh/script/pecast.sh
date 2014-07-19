c1bb9f84dcdbfd26b623e124cf6a9f1a4d2c91a5
mytoh <mytoh@gmail.com>
2014-07-19 22:37:36 +0900

    use mone-service

--- bottles/mytoh/script/pecast.sh
+++ bottles/mytoh/script/pecast.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-exec mono ~/huone/ohjelmat/peercaststation/PeerCastStation.exe
+exec mono-service -d:/home/mytoh/huone/ohjelmat/peercaststation -m:PeerCastStation ~/huone/ohjelmat/peercaststation/PeerCastStation.exe &&
+#exec mono ~/huone/ohjelmat/peercaststation/PeerCastStation.exe &
 #exec mono --llvm --server ~/huone/ohjelmat/peercaststation/PeerCastStation.exe
